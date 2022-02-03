fec5a2b0b3ba2fe4d6b21a90d9d8d56f63549dc8
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-02-03 05:35:40 +0500

    Adding React Native Navigation

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false,}}/>
+            <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false,}}/>
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
