9f936d990e25323d5dfd7ea1002993c52369cc7b
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-02-05 23:49:21 +0500

    Dispatching data for MapScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from "tailwind-react-native-classnames"
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env"
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = ({navigation}) => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -14,11 +18,16 @@ const HomeScreen = ({navigation}) => {
             styles={{container: {flex: 0,}, textInput: {fontSize: 18,}, height: 100}}
             nearbyPlacesAPI='GooglePlacesSearch' 
             debounce={400}
+            fetchDetails={true}
             placeholder="Where From?"
             enablePoweredByContainer={false}
+            returnKeyType={"search"}
             onPress={(data, details = null) => {
-              console.log(data);
-              console.log(details);
+              dispatch(setOrigin({
+                location: details.geometry.location,
+                description: data.description
+              }))
+              dispatch(setDestination(null))
             }}
             query={{
               key: GOOGLE_MAPS_APIKEY,
