ea5327095dd83222374395c868fa9437d26f4558
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-02-05 22:21:37 +0500

    Adding React Navigation

--- App.js
+++ App.js
@@ -4,14 +4,25 @@ import { Provider } from "react-redux"
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 import { SafeAreaProvider } from "react-native-safe-area-context"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen/>
-        <StatusBar style="auto" />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false,}}/>
+            <Stack.Screen name='MapScreen' component={MapScreen} options={{headerShown: false,}}/>
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </SafeAreaProvider>
+      </NavigationContainer>
+      
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
   {
@@ -19,13 +20,15 @@ const data = [
 ]
 
 const NavOptions = () => {
+  const navigation = useNavigation()
+
   return (
     <FlatList 
       data={data} 
       keyExtractor={(item) => item.id} 
       horizontal 
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} onPress={() => navigation.navigate(item.screen)}>
           <View>
             <Image style={{width: 120, height: 120, resizeMode: "contain"}} source={{uri: item.image}}/>
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
