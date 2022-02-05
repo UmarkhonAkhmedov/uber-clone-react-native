b78c93d70d7f634a0da20841b5eb149f5c3a4a55
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-02-05 23:40:11 +0500

    Adding Google Auto Complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native';
 import React from 'react';
 import tw from "tailwind-react-native-classnames"
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env"
 
 const HomeScreen = ({navigation}) => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image style={{width: 100, height: 100, resizeMode: 'contain'}} source={{uri: "https://links.papareact.com/gzs"}}/>
+        <GooglePlacesAutocomplete 
+            styles={{container: {flex: 0,}, textInput: {fontSize: 18,}, height: 100}}
+            nearbyPlacesAPI='GooglePlacesSearch' 
+            debounce={400}
+            placeholder="Where From?"
+            enablePoweredByContainer={false}
+            onPress={(data, details = null) => {
+              console.log(data);
+              console.log(details);
+            }}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'en',
+            }}
+            />
         <NavOptions/>
       </View>
     </SafeAreaView>
