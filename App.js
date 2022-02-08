626cb78d0bfdb46feeb8cebcf8724c185afb4775
UmarkhonAkhmedov <76143018+UmarkhonAkhmedov@users.noreply.github.com>
2022-02-08 23:49:58 +0500

    Deleting styling in App js file

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { Provider } from "react-redux"
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
@@ -26,12 +25,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
